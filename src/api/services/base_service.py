43013fde20f067b89d87da989825d14e3655914b
nick <8650180292@my.lerna.by>
2025-12-19 12:57:10 +0300

    update base_service.py

--- src/api/services/base_service.py
+++ src/api/services/base_service.py
@@ -5,12 +5,10 @@
 и валидацию данных перед передачей в репозиторий.
 """
 
-from typing import Generic, TypeVar, cast
+from typing import Generic, TypeVar
 
 from pydantic import BaseModel
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.core.logging import log
 from src.api.models import Base as SQLAlchemyBaseModel
 from src.api.repositories import BaseRepository
 
@@ -42,37 +40,3 @@ class BaseService(Generic[ModelType, RepositoryType, CreateSchemaType, UpdateSch
             repository (RepositoryType): Репозиторий для работы с данными.
         """
         self.repository = repository
-
-    async def create(self, db_session: AsyncSession, *, obj_in: CreateSchemaType) -> ModelType:
-        """
-        Создает новый объект.
-
-        Управляет транзакцией: выполняет commit в случае успеха или rollback при ошибке.
-
-        Args:
-            db_session (AsyncSession): Асинхронная сессия базы данных.
-            obj_in (CreateSchemaType): Схема с данными для создания.
-
-        Returns:
-            ModelType: Созданный объект.
-        """
-
-        model_name = self.repository.model.__name__
-
-        try:
-            # Репозиторий добавляет объект в сессию
-            db_obj = await self.repository.create(db_session, obj_in=obj_in)
-
-            # Сервис фиксирует транзакцию (бизнес-операция завершена успешно)
-            await db_session.commit()
-
-            # Возвращаем созданный объект
-            return cast(ModelType, db_obj)  # Явное приведение типа для mypy
-
-        except Exception as exc:
-            # При любой ошибке откатываем транзакцию, чтобы сохранить целостность данных
-            await db_session.rollback()
-
-            # Логируем ошибку и выбрасываем исключение
-            log.error(f"Ошибка при создании {model_name}: {exc}", exc_info=True)
-            raise exc

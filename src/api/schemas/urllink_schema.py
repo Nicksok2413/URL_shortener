e74aa120d531a50508a1175c6c772d1a5f5af8f6
nick <8650180292@my.lerna.by>
2025-12-19 12:37:30 +0300

    update urllink_schema.py

--- src/api/schemas/urllink_schema.py
+++ src/api/schemas/urllink_schema.py
@@ -5,14 +5,21 @@ from pydantic import Field, HttpUrl
 from .base_schema import BaseSchema
 
 
+class UrlLinkBase(BaseSchema):
+    """Базовая схема ссылки."""
+    original_url: str = Field(..., description="Оригинальная пользовательская ссылка")
+    short_code: str = Field(..., description="Шорт код (случайно сгенерированная строка)")
+
+
 class UrlLinkCreate(BaseSchema):
     """Схема для создания новой ссылки (данные от пользователя)."""
-
     url: HttpUrl = Field(..., description="Ссылка от пользователя")
 
 
-class UrlLinkResponse(BaseSchema):
-    """"""
+class UrlLinkResponse(UrlLinkBase):
+    """
+    Схема ответа пользователю.
 
-    short_code: str = Field(..., description="")
-    original_url: HttpUrl = Field(..., description="")
+    Наследует поля от базовой схемы ссылки.
+    """
+    short_url: str = Field(..., description="Новая короткая ссылка")
